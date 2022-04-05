75ccef6130a0dcb9f4ccaacd730a14209b658e08
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2022-04-05 16:17:46 +0900

    Task4修正

--- src/course/the3rd/Task4.java
+++ src/course/the3rd/Task4.java
@@ -7,19 +7,15 @@ public class Task4 {
 	public static void main(String[] args) {
 		File file = new File("sample.txt");
 		try {
-
+			
 //			例外が発生する可能性のあるコード
 			file.createNewFile();
 		} catch (IOException e) {
-
+			
 //			IOExceptionOがスローされてきた時の対処方法
 //			do something
 		}
 		System.out.println("finish");
 	}
-<<<<<<< HEAD
-}
-=======
 
 }
->>>>>>> main
