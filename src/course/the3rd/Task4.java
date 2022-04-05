0c35f884c1380ff78a9b29dce42f9f2d78fd4351
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2022-04-05 11:47:08 +0900

    例外処理を作成

--- src/course/the3rd/Task4.java
+++ src/course/the3rd/Task4.java
@@ -1,47 +1,20 @@
 package course.the3rd;
 
 import java.io.File;
-<<<<<<< HEAD
 import java.io.IOException;
 
 public class Task4 {
 	public static void main(String[] args) {
 		File file = new File("sample.txt");
 		try {
-			
+
 //			例外が発生する可能性のあるコード
 			file.createNewFile();
 		} catch (IOException e) {
-			
+
 //			IOExceptionOがスローされてきた時の対処方法
 //			do something
 		}
 		System.out.println("finish");
 	}
-
 }
-=======
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-
-public class Task4 {
-
-	public static void main(String[] args) {
-		File file = new File("sample.txt");
-
-		try {
-			file.createNewFile();
-			FileWriter writer = new FileWriter(file);
-//			例外が発生する可能性があるコード
-			writer.write("test");
-
-		} catch (FileNotFoundException e) {
-//			FileNotFoundExceptionがスローされてきた時の対処方法
-//			do something
-		} catch (IOException e) {
-//		    do something
-		}
-	}
-}
->>>>>>> origin/main
