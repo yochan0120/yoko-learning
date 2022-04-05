df57ee4e6ed4b7990fc8501f5797e2bc44eeefd7
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2022-04-05 11:29:22 +0900

    コンフリクト修正

--- src/course/the3rd/Task3.java
+++ src/course/the3rd/Task3.java
@@ -1,43 +1,23 @@
 package course.the3rd;
 
-<<<<<<< HEAD
 public class Task3 {
 	public static void main(String[] args) {
 		System.out.println("プログラムの実行");
-		
-		try{
+
+		try {
 			System.out.println("IndexOutOfBoundsExtentionを意図的に発生");
 			int[] a = new int[2];
-			for(int i = 0; i <= 2; i++) {
+			for (int i = 0; i <= 2; i++) {
 				a[i] = i;
 			}
 			System.out.println("例外発生せず");
-			} catch(NullPointerException e) {
-				System.out.println(e.getMessage());
-				System.out.println("NullPointerExceltion発生");
-			} catch(Exception e) {
-				System.out.println(e.getMessage());
-				System.out.println("Exception発生");
+		} catch (NullPointerException e) {
+			System.out.println(e.getMessage());
+			System.out.println("NullPointerExceltion発生");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("Exception発生");
 		}
 	}
 
-=======
-import java.io.File;
-import java.io.IOException;
-
-public class Task3 {
-	public static void main(String[] args) {
-		File file = new File("sample.txt");
-		try {
-
-//			例外が発生する可能性のあるコード
-			file.createNewFile();
-		} catch (IOException e) {
-
-//			IOExceptionOがスローされてきた時の対処方法
-//			do something
-		}
-		System.out.println("finish");
-	}
->>>>>>> origin/main
 }
--- src/course/the3rd/Task5.java
+++ src/course/the3rd/Task5.java
@@ -1,6 +1,5 @@
 package course.the3rd;
 
-<<<<<<< HEAD
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -12,7 +11,7 @@ public class Task5 {
 		File file = new File("sample.txt");
 
 		try {
-			file.createNewFile(); 
+			file.createNewFile();
 			FileWriter writer = new FileWriter(file);
 //			例外が発生する可能性があるコード
 			writer.write("test");
@@ -25,26 +24,3 @@ public class Task5 {
 		}
 	}
 }
-=======
-public class Task5 {
-	public static void main(String[] args) {
-		System.out.println("A");
-		try {
-			int[] array = null;
-			System.out.println("B");
-
-//			参照先がないのに０番目の要素の値を代入しようとしている
-			array[0] = 10;
-			System.out.println("C");
-		} catch (NullPointerException e) {
-			System.out.println("D");
-			// TODO: handle exception
-		} finally {
-			System.out.println("E");
-		}
-		System.out.println("F");
-
-	}
-
-}
->>>>>>> origin/main
